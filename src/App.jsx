adceeeb9803b09cee90dd3939394d26028ce6c39
Rohan Gajurel <rohangajurel18@gmail.com>
2024-06-09 15:08:11 +0545

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header></Header>
+      <Footer></Footer>
+    </>
+  );
+}
+
+export default App;
